fc244e6c08eecab508050a6b210938377f58000d
Scott Chamberlain <myrmecocystus@gmail.com>
2016-03-08 00:23:05 +0000

    version 0.1.1

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,11 @@
+scrubr 0.1.1
+============
+
+### MINOR IMPROVEMENTS
+
+* Fixed examples to be conditional on presence of `rgbif` (#17)
+* Fix `as.matrix()` to use `Matrix::as.matrix()`
+
 scrubr 0.1.0
 ============
 
--- R/scrubr-package.R
+++ R/scrubr-package.R
@@ -5,24 +5,11 @@
 #' @importFrom utils head data
 #' @importFrom lazyeval lazy_dots all_dots
 #' @importFrom qlcMatrix sim.strings
+#' @importFrom Matrix as.matrix
 #' @importFrom magrittr %>%
 #' @name scrubr-package
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @aliases scrubr
 #' @docType package
 #' @keywords package
-NULL
-
-#' Sample data.frame number 1
-#'
-#' @name sample_data_1
-#' @docType data
-#' @keywords datasets
-NULL
-
-#' Sample data.frame number 2
-#'
-#' @name sample_data_2
-#' @docType data
-#' @keywords datasets
 NULL
